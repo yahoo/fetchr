cc6ba67be7413877f9b9f6a624ee8bd6f13fe377
Rajiv Tirumalareddy <rajivtirum@yahoo-inc.com>
2015-04-01 14:08:02 -0700

    [Fixes #66] Use JSON.parse on query string values in case they are arrays or objects
    Added supertest[#63] to test integration between fetcher.js and fetcher.client.js

---
+++ tests/mock/app.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2014, Yahoo! Inc.
+ * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
+ */
+
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var Fetcher = require('../../libs/fetcher.js');
+var mockService = require('./MockService');
+var mockErrorService = require('./MockErrorService');
+
+Fetcher.registerFetcher(mockService);
+Fetcher.registerFetcher(mockErrorService);
+
+var app = express();
+app.use(bodyParser.json());
+app.use('/api', Fetcher.middleware());
+var port = process.env.PORT || 3000;
+var server = http.createServer(app).listen(port);
+console.log('Listening on port ' + port);
+module.exports = server;
+module.exports.cleanup = function () {
+    Fetcher.fetchers = {};
+};
