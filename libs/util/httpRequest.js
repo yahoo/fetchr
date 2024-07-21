74ca779c88307d9822b47c2e3aeb83d295c3ca65
Pablo Palacios <pablo.palacios@holidaycheck.com>
2024-07-21 11:00:42 +0200

    ref: convert httpRequest into FetchrHttpRequest class

--- libs/util/httpRequest.js
+++ libs/util/httpRequest.js
@@ -9,148 +9,161 @@
 
 var FetchrError = require('./FetchrError');
 
-function _shouldRetry(err, options, attempt) {
+function _shouldRetry(err) {
     if (err.reason === FetchrError.ABORT) {
         return false;
     }
 
-    if (attempt >= options.retry.maxRetries) {
+    if (this._currentAttempt >= this._options.retry.maxRetries) {
         return false;
     }
 
-    if (options.method === 'POST' && !options.retry.retryOnPost) {
+    if (this._options.method === 'POST' && !this._options.retry.retryOnPost) {
         return false;
     }
 
-    return options.retry.statusCodes.indexOf(err.statusCode) !== -1;
+    return this._options.retry.statusCodes.indexOf(err.statusCode) !== -1;
 }
 
-function _fetch(options, controller) {
+// _retry is the onReject promise callback that we attach to the
+// _fetch call (ex. _fetch().catch(_retry)). Since _fetch is a promise
+// and since we must be able to retry requests (aka call _fetch
+// function again), we must call _fetch from within _retry. This means
+// that _fetch is a recursive function. Recursive promises are
+// problematic since they can block the main thread for a
+// while. However, since the inner _fetch call is wrapped in a
+// setTimeout we are safe here.
+//
+// The call flow:
+//
+// send -> _fetch -> _retry -> _fetch -> _retry -> end
+function _retry(err) {
+    var self = this;
+    if (!_shouldRetry.call(self, err)) {
+        throw err;
+    }
+
+    // Use exponential backoff and full jitter
+    // strategy published in
+    // https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
+    var delay =
+        Math.random() *
+        self._options.retry.interval *
+        Math.pow(2, self._currentAttempt);
+
+    self._controller = new AbortController();
+    self._currentAttempt += 1;
+
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            _fetch.call(self).then(resolve, reject);
+        }, delay);
+    });
+}
+
+function _fetch() {
+    var self = this;
     var timedOut = false;
-    var request = new Request(options.url, {
-        body: options.body,
-        credentials: options.credentials,
-        headers: options.headers,
-        method: options.method,
-        signal: controller.signal,
+    var request = new Request(self._options.url, {
+        body: self._options.body,
+        credentials: self._options.credentials,
+        headers: self._options.headers,
+        method: self._options.method,
+        signal: self._controller.signal,
     });
 
     var timeoutId = setTimeout(function () {
         timedOut = true;
-        controller.abort();
-    }, options.timeout);
+        self._controller.abort();
+    }, self._options.timeout);
 
-    return fetch(request).then(
-        function (response) {
-            clearTimeout(timeoutId);
+    return fetch(request)
+        .then(
+            function (response) {
+                clearTimeout(timeoutId);
 
-            if (response.ok) {
-                return response.json().catch(function () {
+                if (response.ok) {
+                    return response.json().catch(function () {
+                        throw new FetchrError(
+                            FetchrError.BAD_JSON,
+                            'Cannot parse response into a JSON object',
+                            self._options,
+                            request,
+                            response,
+                        );
+                    });
+                } else {
+                    return response.text().then(function (message) {
+                        throw new FetchrError(
+                            FetchrError.BAD_HTTP_STATUS,
+                            message,
+                            self._options,
+                            request,
+                            response,
+                        );
+                    });
+                }
+            },
+            function (err) {
+                clearTimeout(timeoutId);
+                if (err.name === 'AbortError') {
+                    if (timedOut) {
+                        throw new FetchrError(
+                            FetchrError.TIMEOUT,
+                            'Request failed due to timeout',
+                            self._options,
+                            request,
+                        );
+                    }
+
                     throw new FetchrError(
-                        FetchrError.BAD_JSON,
-                        'Cannot parse response into a JSON object',
-                        options,
-                        request,
-                        response,
-                    );
-                });
-            } else {
-                return response.text().then(function (message) {
-                    throw new FetchrError(
-                        FetchrError.BAD_HTTP_STATUS,
-                        message,
-                        options,
-                        request,
-                        response,
-                    );
-                });
-            }
-        },
-        function (err) {
-            clearTimeout(timeoutId);
-            if (err.name === 'AbortError') {
-                if (timedOut) {
-                    throw new FetchrError(
-                        FetchrError.TIMEOUT,
-                        'Request failed due to timeout',
-                        options,
+                        FetchrError.ABORT,
+                        err.message,
+                        self._options,
                         request,
                     );
                 }
 
                 throw new FetchrError(
-                    FetchrError.ABORT,
+                    FetchrError.UNKNOWN,
                     err.message,
-                    options,
+                    self._options,
                     request,
                 );
-            }
-
-            throw new FetchrError(
-                FetchrError.UNKNOWN,
-                err.message,
-                options,
-                request,
-            );
-        },
-    );
+            },
+        )
+        .catch(_retry.bind(self));
 }
 
+function _send() {
+    this._promise = _fetch.call(this);
+}
+
+function FetchrHttpRequest(options) {
+    this._controller = new AbortController();
+    this._currentAttempt = 0;
+    this._options = options;
+    this._promise = null;
+}
+
+FetchrHttpRequest.prototype.abort = function () {
+    return this._controller.abort();
+};
+
+FetchrHttpRequest.prototype.then = function (resolve, reject) {
+    this._promise = this._promise.then(resolve, reject);
+    return this;
+};
+
+FetchrHttpRequest.prototype.catch = function (reject) {
+    this._promise = this._promise.catch(reject);
+    return this;
+};
+
 function httpRequest(options) {
-    var controller = new AbortController();
-    var currentAttempt = 0;
-
-    // _retry is the onReject promise callback that we attach to the
-    // _fetch call (ex. _fetch().catch(_retry)). Since _fetch is a
-    // promise and since we must be able to retry requests (aka call
-    // _fetch function again), we must call _fetch from within
-    // _retry. This means that _fetch is a recursive
-    // function. Recursive promises are problematic since they can
-    // block the main thread for a while. However, since the inner
-    // _fetch call is wrapped in a setTimeout we are safe here.
-    //
-    // The call flow:
-    //
-    // httpRequest -> _fetch -> _retry -> _fetch -> _retry -> end
-    function _retry(err) {
-        if (!_shouldRetry(err, options, currentAttempt)) {
-            throw err;
-        }
-
-        // Use exponential backoff and full jitter
-        // strategy published in
-        // https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
-        var delay =
-            Math.random() *
-            options.retry.interval *
-            Math.pow(2, currentAttempt);
-
-        controller = new AbortController();
-        currentAttempt += 1;
-
-        return new Promise(function (resolve, reject) {
-            setTimeout(function () {
-                _fetch(options, controller).catch(_retry).then(resolve, reject);
-            }, delay);
-        });
-    }
-
-    var promise = _fetch(options, controller).catch(_retry);
-
-    return {
-        then: promise.then.bind(promise),
-        catch: promise.catch.bind(promise),
-
-        // Differently from then and catch, we must wrap
-        // controller.abort in our own function to make sure that we
-        // have a fresh reference to the current AbortController being
-        // used. If we don't do it, controller will point to the first
-        // request, preventing users to abort subsequent requests in
-        // case of retries.
-        abort: function () {
-            return controller.abort();
-        },
-    };
+    var request = new FetchrHttpRequest(options);
+    _send.call(request);
+    return request;
 }
 
 module.exports = httpRequest;
